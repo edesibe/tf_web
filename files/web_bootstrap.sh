371ef0f8278cdee8e579b11be1624fcc3c7f14b4
Ivan Milivojevic <ivan.milivojevic@endava.com>
2019-03-02 00:06:22 +0100

    update

--- files/web_bootstrap.sh
+++ files/web_bootstrap.sh
@@ -3,13 +3,13 @@ sudo yum update -y
 sudo yum install -y nginx
 
 # Added index.html
-sudo cat >/var/www/html/index.html << "EOF"
+sudo cat >/usr/share/nginx/html/index.html << "EOF"
 <html>
   <head>
     <title>Web service</title>
   </head>
   <body>
-    <h1>The Terraform Book Web service</h1>
+    <h1>The Web service</h1>
   </body>
 </html>
 EOF
